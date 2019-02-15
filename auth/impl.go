28840d82a3e703c1acfc087fa2fd261a556b027b
Ernest <ernest@csie.org>
2019-02-15 22:55:21 +0800

    add auth service boilerplate

---
+++ auth/impl.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+
+	authapi "github.com/chiaen/usr/api/auth"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	errUnimplemented = status.Error(codes.Unimplemented, "not implement yet")
+)
+
+type serviceImpl struct {
+}
+
+func newAuthService() (authapi.AuthenticationServer, error) {
+	return &serviceImpl{}, nil
+}
+
+func (s *serviceImpl) SignupNewUser(ctx context.Context, req *authapi.PasswordRequest) (*authapi.TokenResponse, error) {
+	return nil, errUnimplemented
+}
+
+func (s *serviceImpl) SignInWithPassword(ctx context.Context, req *authapi.PasswordRequest) (*authapi.TokenResponse, error) {
+	return nil, errUnimplemented
+}
---
+++ auth/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/chiaen/usr/api/auth"
+	"google.golang.org/grpc"
+)
+
+const (
+	defaultPort = "80"
+)
+
+var (
+	exposing = kingpin.Flag("port", "exposing port of server").Default(defaultPort).Short('p').String()
+)
+
+func main() {
+	l, err := net.Listen("tcp", *exposing)
+	if err != nil {
+		log.Fatalf("listen to port %s failed: %v", *exposing, err)
+	}
+	server := grpc.NewServer()
+	impl, err := newAuthService()
+	if err != nil {
+		log.Fatalf("cannot init api server : %v", err)
+	}
+	auth.RegisterAuthenticationServer(server, impl)
+	if err := server.Serve(l); err != nil {
+		log.Printf("server execution err: %v", err)
+		os.Exit(1)
+	}
+}
